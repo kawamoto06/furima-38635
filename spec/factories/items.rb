48fc7e64d111c9e89f8518936ddee8c4e1eba46f
kawamoto06 <kawamoto0605@gmail.com>
2022-11-10 17:39:46 +0900

    javaの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
+    image         {Faker::Lorem.sentence}
+    name          {"名前"}
+    text          {"説明"}
+    price              {2000}
+    condition_id       {2}
+    category_id        {2}
+    prefecture_id      {2}
+    shipping_cost_id   {2}
+    shipping_date_id   {2}
+    association :user
     
   end
 end
