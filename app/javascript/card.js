7602d5af125a91441561f88aaa0bba5355f09c27
kawamoto06 <kawamoto0605@gmail.com>
2023-01-11 14:05:16 +0900

    修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,7 @@
 const pay = () => {
-  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY)
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
   const elements = payjp.elements();
-  
+
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
